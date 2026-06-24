98596d6da7299c54817cb9d14d00dca9cd71e315
VoltaReborn <thecollinsk@gmail.com>
2026-06-24 16:08:40 -0500

    layout

--- sw.js
+++ sw.js
@@ -1,50 +1,14 @@
-const CACHE_NAME = 'what-the-heck-pwa-v82';
-const APP_SHELL = [
-  './',
-  './index.html',
-  './manifest.webmanifest'
-];
-
+const CACHE_NAME = 'what-the-heck-pwa-v83';
+const APP_SHELL = ['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(APP_SHELL))
-      .then(() => self.skipWaiting())
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
 });
-
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.map(key => key === CACHE_NAME ? null : caches.delete(key))))
-      .then(() => self.clients.claim())
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => key === CACHE_NAME ? null : caches.delete(key)))).then(() => self.clients.claim()));
 });
-
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request)
-        .then(response => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put('./index.html', copy)).catch(() => {});
-          return response;
-        })
-        .catch(() => caches.match('./index.html'))
-    );
-    return;
-  }
-
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      if (cached) return cached;
-      return fetch(event.request).then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => {});
-        return response;
-      });
-    })
-  );
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).catch(() => {
+    if (event.request.mode === 'navigate') return caches.match('./index.html');
+  })));
 });
