90876b6b419aa3200df5e51018c21328e14f2fbf
VoltaReborn <thecollinsk@gmail.com>
2026-06-24 21:06:45 -0500

    version fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'what-the-heck-pwa-v121';
+const CACHE_NAME = 'what-the-heck-pwa-v122';
 const APP_SHELL = ['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
