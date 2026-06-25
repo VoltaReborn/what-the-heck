6ec8821436d302641fb786d665c9bac8076250f6
VoltaReborn <thecollinsk@gmail.com>
2026-06-24 22:32:34 -0500

    continue game fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'what-the-heck-pwa-v131';
+const CACHE_NAME = 'what-the-heck-pwa-v132';
 const APP_SHELL = ['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
